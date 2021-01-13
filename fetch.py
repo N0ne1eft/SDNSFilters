68a3bf90fdd98eef5b664b905abef9497fc14b4f
Noneleft <noneleftzyx@gmail.com>
2021-01-13 09:23:21 +0000

    QXupdate

--- fetch.py
+++ fetch.py
@@ -8,12 +8,23 @@ except:
 rules = r.content.decode()
 f.write(rules)
 f.close()
+
 srules = rules.replace('||','DOMAIN-SUFFIX,')
 srules = srules.replace('^','')
 srules = srules.replace('!','#')
+
+qrules = rules.replace('||','HOST-SUFFIX,')
+qrules = qrules.replace('^',',Advertising')
+qrules = qrules.replace('!',';')
 try:
     f = open('srocket.txt',mode='x')
 except:
     f = open('srocket.txt', mode='w')
 f.write(srules)
+f.close()
+try:
+    f = open('qx.txt', mode='x')
+except:
+    f = open('qx.txt', mode='w')
+f.write(qrules)
 f.close()
